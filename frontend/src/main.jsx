294bfb3db5be2db091c99187167eac6ac21b4743
rebeccafitzpatr <rfit696@aucklanduni.ac.nz>
2024-12-20 19:28:26 +1300

    completed frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,15 @@
-import { StrictMode } from 'react'
-import App from './App'
-import ReactDOM from 'react-dom'
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
-import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router-dom'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
 		<BrowserRouter>
 			<ChakraProvider>
 				<App />
 			</ChakraProvider>
 		</BrowserRouter>
 	</React.StrictMode>
-    
 );
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react'
+import { Container, SimpleGrid, Text, VStack } from '@chakra-ui/react'
+import { Link } from 'react-router-dom';
+import { useProductStore } from '../store/product';
+import ProductCard from '../components/ProductCard';
+
+const HomePage = () => {
+    const {fetchProducts, products} = useProductStore();
+    useEffect(() => {
+        fetchProducts();
+    }, [fetchProducts]);
+    console.log("products", products);
+
+  return (
+    <Container maxW='container.x1' py ={12}>
+        <VStack spacing={8}>
+            <Text
+                fontSize = {"30"}
+                fontWeight = {"bold"}
+                bgGradient={"linear(to-r, #29EECA, #B715DA)"}
+                bgClip={"text"}
+                textAlign={"center"}
+                >
+                Current Products
+                </Text>
+
+                <SimpleGrid
+                    columns={{
+                        base: 1,
+                        md: 2,
+                        lg: 3,
+                    }}
+
+                    spacing={10}
+                    w={"full"}
+                >
+                    {products.map((product) => (
+                        <ProductCard key={product._id} product={product}/>
+                    ))}
+
+                </SimpleGrid>
+
+                {products.length > 0 && (
+
+                <Text fontSize ='x1' textAlign={"center"} fontWeight='bold' color='gray.500'>
+                    No products available {" "}
+                    <Link to={'/create'}>
+                        <Text as = 'span' color='blue.500' _hover={{ textDecoration: "underline"}}>
+                         Create 
+                        </Text>
+                    </Link>
+                </Text>
+
+        )}
+        </VStack>
+    </Container>
+  )
+};
+
+export default HomePage
