e21e1c945cd6e62ecee16d25e18b7311125f0943
rebeccafitzpatr <rfit696@aucklanduni.ac.nz>
2024-12-20 19:53:31 +1300

    project complete

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import {connectDB} from './config/db.js';
-import Product from "./models/product.model.js"
-import mongoose from 'mongoose';
+
+import path from 'path';
 
 import productRoutes from "./routes/product.route.js";
 
@@ -11,9 +11,20 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 
 app.use("/api/products", productRoutes)
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile( path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    })
+
+}
 
 app.listen(PORT, () => {
     connectDB();
